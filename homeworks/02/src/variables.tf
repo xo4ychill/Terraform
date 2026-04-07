42b9e3e4debd458c315f4dcbbfc8f881b6b39287
LebedevRP <xo4y.chill@yandex.ru>
2026-04-07 15:17:21 +0300

    main

--- homeworks/02/src/variables.tf
+++ homeworks/02/src/variables.tf
@@ -33,6 +33,7 @@ variable "vpc_name" {
 
 variable "vms_ssh_root_key" {
   type        = string
-  default     = "file("~/.ssh/id_rsa.pub")"
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCfEfuBB+/Gz3a2s+HRS96m+W3hKj41JVxNpDpsuFAwix8MrG0tOpGZFH3Hmh1Iy2mCYTacGOaVKgHy3vu6qJkNkWhZjA5ZvJOkaFxzaF/bG+apSD2BdpK2ig4VXHJg0qVjLmQmXCzqg2xhg4P4i++8oY0pPmf2nreN6O8rsIDys2ZNCPPg0I1YFO5wi6JMIJiJ/j8CaGUBFjAe0A2nshTpIIB14Zjqmh74KAfHssem5Awf09DwaZeIYgNPLpS++j2xKd1TZqbyxauuUWzbh2h6ERRSVIiLu+kSIfLMZy/rSR5D+qVygvNOlD1xWq8fePcEy4/dAt45w9b0zv9K7pawq88jdf4c1XK4O5aqvAjPS8HGO1c8+Z5qR2WAhRgYlfWSgdz9XgDYTQlcmuzHhhHDBz1ywjmOdHfD6nmBhSB+YFX/m9WO5cZte4O9YVoNnDD+kX4xU6AocdCEzxuKmXy7fDlDZJ9Kf266qS+6/BlC700938Dk9fcn0qaxXYMh3zwj+8gRpYuDxzRD7G+Y8dHBofJs6xtUtYjkO/17N4k7C93+dPYWc+KrlUyEZteCp7VIA111fgNl6zpbHOyyNLd4FTyyTgCyh9VaZUr/iWui0ZoQmdq5OPNJeYH7yXDmLIqS0oV8PNyM7y+JLtJ4GgZj2GTG5Fp/3Tg1WEcjJMg8qQ== xo4y.chill@yandex.ru"
   description = "ssh-keygen -t ed25519"
 }
+
